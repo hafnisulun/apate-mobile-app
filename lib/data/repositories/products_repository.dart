ba41de26305e14e80c004a627183bb8e19d1664c
Hafni Sulun <hafni.sulun@gmail.com>
2022-01-06 20:45:25 +0700

    Simplify refresh token interception

--- lib/data/repositories/products_repository.dart
+++ lib/data/repositories/products_repository.dart
@@ -1,6 +1,5 @@
-import 'package:apate/data/models/login.dart';
 import 'package:apate/data/models/products.dart';
-import 'package:apate/data/repositories/login_repository.dart';
+import 'package:apate/utils/auth.dart';
 import 'package:dio/dio.dart';
 import 'package:path/path.dart';
 
@@ -8,39 +7,10 @@ import '../../constants.dart';
 
 class ProductsRepository {
   Dio _dio = Dio();
-  LoginRepository _loginRepository = LoginRepository();
-  String? _accessToken;
 
   Future<Products?> getProducts(String merchantUuid) async {
-    _accessToken = await _loginRepository.getAccessToken();
-    if (_accessToken == null) {
-      print('[MerchantsRepository] [getProducts] accessToken null');
-      return null;
-    }
-
     try {
-      _dio.interceptors.add(InterceptorsWrapper(
-        onRequest: (options, handler) async {
-          var customHeaders = {'Authorization': 'Bearer ' + _accessToken!};
-          options.headers.addAll(customHeaders);
-          return handler.next(options);
-        },
-        onError: (error, handler) async {
-          if (error.response?.statusCode == 401) {
-            try {
-              Login? login = await _loginRepository.refreshToken();
-              if (login != null) {
-                _accessToken = login.accessToken.token;
-                return handler.resolve(await _retry(error.requestOptions));
-              }
-            } catch (e) {
-              print('[ProductsRepository] [getProducts] exception: $e');
-              return handler.next(error);
-            }
-          }
-          return handler.next(error);
-        },
-      ));
+      _dio.interceptors.add(Auth.getDioInterceptorsWrapper(_dio));
       String url = join(API_BASE_URL, 'products?merchant_uuid=$merchantUuid');
       print('[ProductsRepository] [getProducts] url: $url');
       final response = await _dio.get(Uri.encodeFull(url));
@@ -50,18 +20,5 @@ class ProductsRepository {
       print('[ProductsRepository] [getProducts] exception: ${e.message}');
       throw e;
     }
-  }
-
-  Future<Response<dynamic>> _retry(RequestOptions requestOptions) async {
-    final options = new Options(
-      method: requestOptions.method,
-      headers: requestOptions.headers,
-    );
-    return _dio.request<dynamic>(
-      requestOptions.path,
-      data: requestOptions.data,
-      queryParameters: requestOptions.queryParameters,
-      options: options,
-    );
   }
 }
--- lib/utils/auth.dart
+++ lib/utils/auth.dart
@@ -1,4 +1,7 @@
+import 'package:apate/data/models/login.dart';
+import 'package:apate/data/repositories/login_repository.dart';
 import 'package:apate/screens/login_screen.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class Auth {
@@ -8,6 +11,49 @@ class Auth {
         builder: (BuildContext context) => LoginScreen(),
       ),
       (_) => false,
+    );
+  }
+
+  static InterceptorsWrapper getDioInterceptorsWrapper(Dio dio) {
+    LoginRepository loginRepository = LoginRepository();
+    return InterceptorsWrapper(
+      onRequest: (options, handler) async {
+        String? accessToken = await loginRepository.getAccessToken();
+        if (accessToken != null) {
+          var customHeaders = {'Authorization': 'Bearer ' + accessToken};
+          options.headers.addAll(customHeaders);
+        }
+        return handler.next(options);
+      },
+      onError: (error, handler) async {
+        if (error.response?.statusCode == 401) {
+          try {
+            Login? login = await loginRepository.refreshToken();
+            if (login != null) {
+              return handler
+                  .resolve(await _dioRetry(dio, error.requestOptions));
+            }
+          } catch (e) {
+            print('[ProductsRepository] [getProducts] exception: $e');
+            return handler.next(error);
+          }
+        }
+        return handler.next(error);
+      },
+    );
+  }
+
+  static Future<Response<dynamic>> _dioRetry(
+      Dio dio, RequestOptions requestOptions) async {
+    final options = new Options(
+      method: requestOptions.method,
+      headers: requestOptions.headers,
+    );
+    return dio.request<dynamic>(
+      requestOptions.path,
+      data: requestOptions.data,
+      queryParameters: requestOptions.queryParameters,
+      options: options,
     );
   }
 }
